4a0f0f46f9f42dffb67f58b3d24cdef093dca826
SeanSteberis <103416906+seansteberisal@users.noreply.github.com>
2025-09-15 12:23:49 +0100

    added text to show the license scan did run update pre-commit hook

--- scripts/check_python_licenses.sh
+++ scripts/check_python_licenses.sh
@@ -7,6 +7,7 @@ LICENSES=$(poetry run pip-licenses  --ignore-packages "${IGNORE_PACKAGES}")
 INCOMPATIBLE_LIBS=$(echo "$LICENSES" | grep 'GPL' || true)
 
 if [[ -z $INCOMPATIBLE_LIBS ]]; then
+    echo "The libraries were checked and no license issues were found."
     exit 0
 else
     echo "The following libraries were found which are not compatible with this project's license:"
