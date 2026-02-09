bd14463fa6040eb5f3e393e2a264ac6841f9e5af
adam-wallernhs1 <176666249+adam-wallernhs1@users.noreply.github.com>
2026-02-09 14:19:39 +0000

    Chore: [ELI-9875] - allow the consumer mapping config in preprod (#125)
    allow the consumer mapping config in preprod

--- utils/s3_config_manager.py
+++ utils/s3_config_manager.py
@@ -167,15 +167,11 @@ def delete_all_configs_from_s3() -> None:
 
 
 def upload_consumer_mapping_file_to_s3(local_path: str) -> None:
-    environment = os.getenv("ENVIRONMENT")
-    if environment in ["dev", "test"]:
-        s3_bucket = os.getenv("S3_CONSUMER_MAPPING_BUCKET_NAME")
-        logger.info(
-            "Uploading consumer mapping file: %s to S3 bucket: %s",
-            local_path,
-            s3_bucket,
-        )
-        s3_connection = S3ConfigManager(s3_bucket)
-        s3_connection.upload_if_missing_or_changed(Path(local_path))
-    else:
-        logger.info("Skipping upload in preprod until feature deployed")
+    s3_bucket = os.getenv("S3_CONSUMER_MAPPING_BUCKET_NAME")
+    logger.info(
+        "Uploading consumer mapping file: %s to S3 bucket: %s",
+        local_path,
+        s3_bucket,
+    )
+    s3_connection = S3ConfigManager(s3_bucket)
+    s3_connection.upload_if_missing_or_changed(Path(local_path))
