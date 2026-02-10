156ba773066dc5268d6291cb1f93cda587bce0d5
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2026-02-10 14:01:35 +0000

    linting

--- tests/performance_tests/locust.py
+++ tests/performance_tests/locust.py
@@ -16,13 +16,14 @@ from utils.eligibility_api_client import EligibilityApiClient
 def _(parser):
     parser.add_argument("--env", choices=["dev", "test", "pre-prod"], default="dev", help="Environment")
 
+
 with open("temp/nhs_numbers.csv", newline='') as csvFile:
     reader = csv.reader(csvFile)
     csvData = list(reader)
 
-#Class for API execution
-class GetPatientId(HttpUser):
 
+# Class for API execution
+class GetPatientId(HttpUser):
     # Required to prevent warnings on certain exceptions with TLS
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -31,22 +32,24 @@ class GetPatientId(HttpUser):
 
     # This can be set in the CLI settings if required to be changed
     PROJECT_ROOT = Path(__file__).resolve().parents[2]  # adjust depth if needed
-    client = EligibilityApiClient(cert_dir= f"{PROJECT_ROOT}/certs")
+    client = EligibilityApiClient(cert_dir=f"{PROJECT_ROOT}/certs")
 
     @task
     def get_patient_data(self):
-
         # Gets a new random NHS Number from provided CSV
         csv_row = random.choice(csvData)
         patient_id = csv_row[0]
 
         # The request is getting sent is here
         with self.client.make_request(
-            patient_id,
-            headers={"Accept" : "application/json", "nhs-login-nhs-number": f"{patient_id}", "NHSE-Product-ID": "P.WTJ-FJT"},
-            raise_on_error=False,
+                patient_id,
+                headers={"Accept": "application/json",
+                         "nhs-login-nhs-number": f"{patient_id}",
+                         "NHSE-Product-ID": "P.WTJ-FJT"},
+                raise_on_error=False,
         ) as response:
             # This is checking asserting the response has the word cohortText, which shows on a valid request
             if 'processedSuggestions' not in response.text:
-                response.failure(f"Response didn't contain processedSuggestions (expected), nhsNumber was {patient_id}. Response was {response.text}")
-
+                response.failure(
+                    f"Response didn't contain processedSuggestions (expected), nhsNumber was {patient_id}. "
+                    f"Response was {response.text}")
