48379eb2be03d58ce2498007a8da3bf2e7c0dc32
adam-wallernhs1 <176666249+adam-wallernhs1@users.noreply.github.com>
2026-02-10 17:02:30 +0000

    Chore: [ELI-9999] - Ensure NBS and My Vaccs configs Remain in preprod after test runs (#130)
    * updated make file to include the upload of the integration test configs
    added the locations of the current int test configs in test_config.py
    updated upload_configs_to_s3 to allow full file paths in the arguments so the above could be passed
    
    * updated make file to include the upload of the integration test configs
    added the locations of the current int test configs in test_config.py
    updated upload_configs_to_s3 to allow full file paths in the arguments so the above could be passed
    
    * updated make file to include the upload of the integration test configs
    added the locations of the current int test configs in test_config.py
    updated upload_configs_to_s3 to allow full file paths in the arguments so the above could be passed
    
    * updated the My Vaccines RSV config to v1.2 at their request

---
+++ tests/test_upload_consumer_configs.py
@@ -0,0 +1,7 @@
+from tests import test_config
+from utils.s3_config_manager import upload_configs_to_s3
+
+
+def test_upload_current_configs():
+    configs = test_config.INT_TEST_CONFIG_PATHS
+    upload_configs_to_s3(configs)
