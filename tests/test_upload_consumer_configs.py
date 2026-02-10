e80b35173949e29b38b95ceb437b0907faa25288
Adam Waller <176666249+adam-wallernhs1@users.noreply.github.com>
2026-02-10 14:26:12 +0000

    updated make file to include the upload of the integration test configs added the locations of the current int test configs in test_config.py updated upload_configs_to_s3 to allow full file paths in the arguments so the above could be passed

--- tests/test_upload_consumer_configs.py
+++ tests/test_upload_consumer_configs.py
@@ -2,6 +2,6 @@ from tests import test_config
 from utils.s3_config_manager import upload_configs_to_s3
 
 
-def test_reset_db():
+def test_upload_current_configs():
     configs = test_config.INT_TEST_CONFIG_PATHS
     upload_configs_to_s3(configs)
